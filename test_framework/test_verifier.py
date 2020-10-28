eed18614ccf06654bcd71f980ad32e291c8772fa
Julian P Samaroo <jpsamaroo@jpsamaroo.me>
2020-10-27 20:54:44 -0500

    Use verifier for tests

---
+++ test_framework/test_verifier.py
@@ -0,0 +1,67 @@
+import os
+import tempfile
+import struct
+from subprocess import Popen, PIPE
+from nose.plugins.skip import Skip, SkipTest
+import ubpf.assembler
+import testdata
+VM = os.path.join(os.path.dirname(os.path.realpath(__file__)), "..", "vm", "test")
+
+def check_datafile(filename):
+    """
+    Given assembly source code and an expected result, run the eBPF program and
+    verify that the result matches.
+    """
+    data = testdata.read(filename)
+    if 'asm' not in data and 'raw' not in data:
+        raise SkipTest("no asm or raw section in datafile")
+    if 'result' not in data and 'verifier error' not in data:
+        raise SkipTest("no result or verifier error section in datafile")
+    if 'error' in data or 'error pattern' in data:
+        raise SkipTest("non-verifier error section in datafile")
+    if not os.path.exists(VM):
+        raise SkipTest("VM not found")
+
+    if 'raw' in data:
+        code = b''.join(struct.pack("=Q", x) for x in data['raw'])
+    else:
+        code = ubpf.assembler.assemble(data['asm'])
+
+    memfile = None
+
+    cmd = [VM]
+    if 'mem' in data:
+        memfile = tempfile.NamedTemporaryFile()
+        memfile.write(data['mem'])
+        memfile.flush()
+        cmd.extend(['-m', memfile.name])
+
+    cmd.extend(['-V', '-'])
+
+    vm = Popen(cmd, stdin=PIPE, stdout=PIPE, stderr=PIPE)
+
+    stdout, stderr = vm.communicate(code)
+    stdout = stdout.decode("utf-8")
+    stderr = stderr.decode("utf-8")
+    stderr = stderr.strip()
+
+    if memfile:
+        memfile.close()
+
+    if 'verifier error' in data:
+        if data['verifier error'] + '\nFailed verification' != stderr:
+            raise AssertionError("Expected error %r, got %r" % (data['verifier error'] + '\nFailed verification', stderr))
+        if vm.returncode == 0:
+            raise AssertionError("Expected VM to exit with an error code")
+    elif 'error' in data or 'error-pattern' in data:
+        if vm.returncode == 0:
+            raise AssertionError("Expected VM to exit with an error code")
+    else:
+        if vm.returncode != 0:
+            raise AssertionError("VM exited with status %d, stderr=%r" % (vm.returncode, stderr))
+
+def test_datafiles():
+    # Nose test generator
+    # Creates a testcase for each datafile
+    for filename in testdata.list_files():
+        yield check_datafile, filename
